a930339c14fa7325c040cfb49e263b66cfd2cb70
claaraamaria <claaraa.maria@yahoo.com>
2020-08-11 10:39:04 +0300

    Passing Data using Properties

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,37 @@ class IssueFilter extends React.Component {
     }
 }
 
+class IssueRow extends React.Component {
+    render() {
+        const style = this.props.rowStyle;
+        return (
+            <tr>
+                <td style={style}>{this.props.issue_id}</td>
+                <td style={style}>{this.props.issue_title}</td>
+            </tr>
+        );
+    }
+}
+
 class IssueTable extends React.Component {
     render() {
+        const rowStyle = {border: "1px solid silver", padding: 4};
         return (
-            <div>This is a placeholder for a table of issues.</div>
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                <tr>
+                    <th style={rowStyle}>ID</th>
+                    <th style={rowStyle}>Title</th>
+                </tr>
+                </thead>
+                <tbody>
+                <IssueRow rowStyle={rowStyle} issue_id={1}
+                          issue_title="Error in console wen clicking Add."/>
+                <IssueRow rowStyle={rowStyle} issue_id={2}
+                          issue_title="Missing bottom border on panel."/>
+                <IssueRow/>
+                </tbody>
+            </table>
         );
     }
 }
@@ -27,16 +54,16 @@ class IssueList extends React.Component {
         return (
             <React.Fragment>
                 <h1>Issue Tracker</h1>
-                <IssueFilter />
-                <hr />
-                <IssueTable />
-                <hr />
-                <IssueAdd />
+                <IssueFilter/>
+                <hr/>
+                <IssueTable/>
+                <hr/>
+                <IssueAdd/>
             </React.Fragment>
         );
     }
 }
 
-const element = <IssueList />;
+const element = <IssueList/>;
 
 ReactDOM.render(element, document.getElementById('content'));
