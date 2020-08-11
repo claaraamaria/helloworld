7e9225f289225045ce644066f4cd9271cec28bb2
claaraamaria <claaraa.maria@yahoo.com>
2020-08-11 11:15:20 +0300

    Passing Data using Children

--- public/App.js
+++ public/App.js
@@ -62,7 +62,7 @@ var IssueRow = /*#__PURE__*/function (_React$Component2) {
         style: style
       }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
         style: style
-      }, this.props.issue_title));
+      }, this.props.children));
     }
   }]);
 
@@ -97,13 +97,11 @@ var IssueTable = /*#__PURE__*/function (_React$Component3) {
         style: rowStyle
       }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 1,
-        issue_title: "Error in console wen clicking Add."
-      }), /*#__PURE__*/React.createElement(IssueRow, {
+        issue_id: 1
+      }, "Error in console wen clicking Add."), /*#__PURE__*/React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 2,
-        issue_title: "Missing bottom border on panel."
-      }), /*#__PURE__*/React.createElement(IssueRow, null)));
+        issue_id: 2
+      }, /*#__PURE__*/React.createElement("div", null, "Missing ", /*#__PURE__*/React.createElement("b", null, "bottom"), " border on panel."))));
     }
   }]);
 
